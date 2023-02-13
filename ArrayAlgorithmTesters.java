99f3f5d05498c77565576f358e765914714df208
Michelle L <mlawson@smith.edu>
2023-02-13 16:05:25 -0500

    testers drafts

--- ArrayAlgorithmTesters.java
+++ ArrayAlgorithmTesters.java
@@ -1,3 +1,145 @@
 public class ArrayAlgorithmTesters {
-    
+    //Helper functions
+    /**
+     * prints an array
+     * @param array array to be printed
+     */
+    public static void printArray(int[] array){
+        System.out.print("[");
+        for(int i=0; i<array.length; i++){
+            System.out.print(array[i]);
+            if(i != array.length-1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    // Testers for ArrayAlgorithm.java
+
+    /**
+     * tests addAll method 
+     */
+    public static void testAddAll(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test addAll() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Sum of array: " + ArrayAlgorithm.addAll(array));
+    }
+
+    /**
+     * tests addAllRec method 
+     */
+    public static void testAddAllRec(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test addAllRec() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Sum of array: " + ArrayAlgorithm.addAllRec(array, array.length));
+    }
+
+    /**
+     * tests multiplyAll method 
+     */
+    public static void testMultiplyAll(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test multiplyAll() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Product of array: " + ArrayAlgorithm.multiplyAll(array));
+    }
+
+    /**
+     * tests multiplyAllRec method 
+     */
+    public static void testMultiplyAllRec(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test multiplyAllRec() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Product of array: " + ArrayAlgorithm.multiplyAllRec(array, array.length));
+    }
+
+    /**
+     * tests isElement method 
+     */
+    public static void testIsElement(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test isElement() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Is 3 in array? " + ArrayAlgorithm.isElement(array, 3));
+        System.out.println("Is 6 in array? " + ArrayAlgorithm.isElement(array, 6));
+    }
+
+    /**
+     * tests isElementRec method 
+     */
+    public static void testIsElementRec(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test isElementRec() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Is 3 in array? " + ArrayAlgorithm.isElementRec(array, 3, array.length));
+        System.out.println("Is 6 in array? " + ArrayAlgorithm.isElementRec(array, 6, array.length));
+    }
+
+    /**
+     * tests findMax method 
+     */
+    public static void testFindMax(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test findMax() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Max of array: " + ArrayAlgorithm.findMax(array));
+    }
+
+    /**
+     * tests findMaxRec method 
+     */
+    public static void testFindMaxRec(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test findMaxRec() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Max of array: " + ArrayAlgorithm.findMaxRec(array, array.length));
+    }
+
+    /**
+     * tests findMin method 
+     */
+    public static void testFindMin(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test findMin() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Min of array: " + ArrayAlgorithm.findMin(array));
+    }
+
+    /**
+     * tests findMinRec method 
+     */
+    public static void testFindMinRec(){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(" ==== Test findMinRec() ====");
+        System.out.println("Array: ");
+        printArray(array);
+        System.out.println("Min of array: " + ArrayAlgorithm.findMinRec(array, array.length));
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        testAddAll();
+        testAddAllRec();
+        testMultiplyAll();
+        testMultiplyAllRec();
+        testIsElement();
+        testIsElementRec();
+        testFindMax();
+        testFindMaxRec();
+        testFindMin();
+        testFindMinRec();
+    }
 }
--- IntAlgorithmTesters.java
+++ IntAlgorithmTesters.java
@@ -1,3 +1,77 @@
 public class IntAlgorithmTesters {
+    // Helper Functions
+    /**
+     * prints an array
+     * @param array array to be printed
+     */
+    public static void printArray(int[] array){
+        System.out.print("[");
+        for(int i=0; i<array.length; i++){
+            System.out.print(array[i]);
+            if(i != array.length-1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    //Testers for IntAlgorithm.java
+    /**
+     * tests sum1toN method
+     */
+    public static void testSum1toN(){
+        System.out.println(" ==== Test sum1toN() ====");
+        System.out.println("Sum of 1 to 5: " + IntAlgorithm.sum1toN(5));
+    }
+
+    /**
+     * tests sum1toNRec method
+     */
+    public static void testSum1toNRec(){
+        System.out.println(" ==== Test sum1toNRec() ====");
+        System.out.println("Sum of 1 to 5: " + IntAlgorithm.sum1toNRec(5));
+    }
+
+    /**
+     * tests product1toN method
+     */
+    public static void testProduct1toN(){
+        System.out.println(" ==== Test product1toN() ====");
+        System.out.println("Product of 1 to 5: " + IntAlgorithm.product1toN(5));
+    }
+
+    /**
+     * tests product1toNRec method
+     */
+    public static void testProduct1toNRec(){
+        System.out.println(" ==== Test product1toNRec() ====");
+        System.out.println("Product of 1 to 5: " + IntAlgorithm.product1toNRec(5));
+    }
+
+    /**
+     * tests gcd method
+     */
+    public static void testGcd(){
+        System.out.println(" ==== Test gcd() ====");
+        System.out.println("GCD of 12 and 18: " + IntAlgorithm.gcd(12, 18));
+    }
+
+    /**
+     * tests gcdRec method
+     */
+    public static void testGcdRec(){
+        System.out.println(" ==== Test gcdRec() ====");
+        System.out.println("GCD of 12 and 18: " + IntAlgorithm.gcdRec(12, 18));
+    }
+
+    //Main method
+    public static void main(String[] args){
+        testSum1toN();
+        testSum1toNRec();
+        testProduct1toN();
+        testProduct1toNRec();
+        testGcd();
+        testGcdRec();
+    }
     
 }
